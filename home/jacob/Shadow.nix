9db6fac256956f40e8821fb68313c7633837c127
PrinceOfDarkness98 <jrschindler2017@icloud.com>
2025-12-20 15:06:35 -0600

    adding hyprland setup and customizing fzf

--- home/jacob/Shadow.nix
+++ home/jacob/Shadow.nix
@@ -4,12 +4,14 @@
     ./home.nix
     ../common
     ../features/cli
+    ../features/desktop
   ];
 
   features = {
     cli = {
       fish.enable = true;
       fzf.enable = true;
+      fastfetch.enable = true;
     };
   };
 }
