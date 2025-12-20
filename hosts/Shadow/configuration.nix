cbe296403e6b1ae5cea7e741ed7144270d64f8db
PrinceOfDarkness98 <jrschindler2017@icloud.com>
2025-12-20 13:39:22 -0600

    removing unnecessary inputs

--- hosts/Shadow/configuration.nix
+++ hosts/Shadow/configuration.nix
@@ -2,13 +2,13 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -38,8 +38,8 @@
   # services.xserver.enable = true;
 
   services.xserver = {
-	enable = true;
-	desktopManager.xfce.enable = true;
+    enable = true;
+    desktopManager.xfce.enable = true;
   };
   services.displayManager.sddm.enable = true;
 
